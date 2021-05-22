3893e46def562b6b5a151f982c4a1dd96428bafc
ludcaron <ludovic.caron31@yahoo.com>
2021-05-22 10:26:25 +0200

    fix

--- src/app/services/cinema.service.ts
+++ src/app/services/cinema.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({providedIn: 'root'})
+export class ServiceNameService {
+  constructor(private httpClient: HttpClient) { }
+
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class CinemaService {
+  public host:string = "http://localhost:8080"
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  public getVilles() {
+    return this.http.get(this.host + "/villes");
+  }
 }
